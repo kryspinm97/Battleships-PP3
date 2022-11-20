cdff1eff79a643356d832a5df2acb254229ab86e
kryspinm97 <maneskryspin69@gmail.com>
2022-11-20 19:03:16 +0000

    Initial Planning

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+Battleships Game against a Computer 
+- Generate ships at random places on board
+- Print a grid sized board (by the user) or a set board
+- Validation of inputs 
+- Score tracking
+
+"""
+
+
+
