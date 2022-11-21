e2c42e9cf78a5cc810e68865469173111669659b
kryspinm97 <maneskryspin69@gmail.com>
2022-11-21 09:01:20 +0000

    Created a print board function

--- run.py
+++ run.py
@@ -4,12 +4,29 @@
 
 """
 Battleships Game against a Computer 
+
 - Generate ships at random places on board
-- Print a grid sized board (by the user) or a set board
+- Print a grid sized board
 - Validation of inputs 
-- Score tracking
+- Score tracking (Hit ships) / Game over if all ships hit.
+- Columns as Letters, Rows as Numbers 
 
 """
 
+import random # Random for the random placement of the ships on the board
+
+
+class Board:
+
+    def __init__(self, board):
+        self.board = board
+    
+
+    def print_board(self):
+        print(" A B C D E F G H I J ")
+        number_of_row = 1
+        for row in self.board:
+            print("%d|%s" % (number_of_row, " | " .join(row))
+            number_of_row += 1
 
 
